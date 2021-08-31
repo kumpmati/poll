fb0b3315aba0fd8267fdd19f5a01d2edf6642cf5
mvkump <mvkump@utu.fi>
2021-08-31 13:18:05 +0300

    fix submission numbers, update poll using websockets

--- src/lib/utils/stats.ts
+++ src/lib/utils/stats.ts
@@ -1,7 +1,10 @@
 import type { Poll } from '$lib/types/poll';
 
 export const calculateStats = (poll: Poll): Record<string, number> => {
-	const stats = { total: 0 };
+	const stats = {
+		totalSubmissions: poll.answers.length,
+		totalSubmissionOptions: 0
+	};
 
 	for (const opt of poll.options) {
 		stats[opt.id] = 0;
@@ -10,7 +13,7 @@ export const calculateStats = (poll: Poll): Record<string, number> => {
 	for (const ans of poll.answers) {
 		for (const opt of ans.options) {
 			stats[opt] += 1;
-			stats.total += 1;
+			stats.totalSubmissionOptions += 1;
 		}
 	}
 
